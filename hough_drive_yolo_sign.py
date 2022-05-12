04a0671d1041d81b9d0b9f1556c66195d589ff18
sybarits <sybarits@gmail.com>
2022-05-13 01:23:01 +0900

    정지 동작 개선 버전
    표지판을 검출하고 정지를 하는 동작을 개선하기 위한 코드 추가
    이 것 역시 상상코딩이므로 검증 필요함

--- hough_drive_yolo_sign.py
+++ hough_drive_yolo_sign.py
@@ -10,13 +10,14 @@
 # 본 프로그램은 상업 라이센스에 의해 제공되므로 무단 배포 및 상업적 이용을 금합니다.
 ####################################################################
 
+import time
 import rospy, rospkg
 import numpy as np
 import cv2, random, math
 from cv_bridge import CvBridge
 from xycar_msgs.msg import xycar_motor
 from sensor_msgs.msg import Image
-from yolov3_trt.msg import BoundingBoxes, BoundingBox
+from yolov3.msg import BoundingBoxes, BoundingBox
 
 from movingAverage import MovingAverage
 
@@ -37,11 +38,11 @@ bridge = CvBridge()
 pub = None
 Width = 640
 Height = 480
-Offset = 370
+Offset = 360
 Gap = 40
 obj_id = -1
 obj_coordinate = None
-detect_size = 352
+detect_size = 415
 
 
 def img_callback(data):
@@ -49,21 +50,36 @@ def img_callback(data):
     image = bridge.imgmsg_to_cv2(data, "bgr8")
 
 
+# def yolo_trt_callback(data) :
+#     global obj_id, obj_coordinate
+#     max_box_size = 0
+#     for bbox in data.bounding_boxes:
+#         # TODO: get max size box
+#         x_size = abs(bbox.xmax - bbox.xmin)
+#         print("bbox ID:", bbox.id, "x_size:",x_size, "xmin:",bbox.xmin,"xmax:",bbox.xmax)
+#         if max_box_size < x_size:
+#             max_box_size = x_size
+#             obj_id = bbox.id
+#             obj_coordinate = {'xmin':min(bbox.xmin,bbox.xmax),
+#                                 'xmax': max(bbox.xmin,bbox.xmax),
+#                                 'ymin': min(bbox.ymin,bbox.ymax),
+#                                 'ymax': max(bbox.ymin,bbox.ymax)}
+
 def yolo_trt_callback(data) :
     global obj_id, obj_coordinate
-    max_box_size = 0
     for bbox in data.bounding_boxes:
-        print(bbox.id)
-        # TODO: get max size box
-        x_size = bbox.xmax - bbox.xmin
-        if max_box_size < x_size:
-            max_box_size = x_size
-            obj_id = bbox.id
-            obj_coordinate = {'xmin':bbox.xmin,
-                                'xmax': bbox.xmax,
-                                'ymin': bbox.ymin,
-                                'ymax': bbox.ymax}
-
+        obj_id = bbox.id
+        if obj_id == 5:
+            size_thrs = 35*85#traffic_thres 40 90, 26 36, 28 39
+        else: 
+            size_thrs = 25 * 35
+        size = (bbox.xmax-bbox.xmin)*(bbox.ymax-bbox.ymin)
+        
+        if size > size_thrs:
+            obj_coordinate = {'xmin':min(bbox.xmin,bbox.xmax),
+                                'xmax': max(bbox.xmin,bbox.xmax),
+                                'ymin': min(bbox.ymin,bbox.ymax),
+                                'ymax': max(bbox.ymin,bbox.ymax)}
 
 # publish xycar_motor msg
 def drive(Angle, Speed): 
@@ -103,10 +119,22 @@ def draw_rectangle(img, lpos, rpos, offset=0):
     return img
 
 
+# draw bbox
+def draw_bbox(img):
+    global Width, Height, detect_size, obj_coordinate
+    if obj_coordinate is None:
+        return
+    xmin = int(obj_coordinate["xmin"]*Width/detect_size)
+    ymin = int(obj_coordinate["ymin"]*Height/detect_size)
+    xmax = int(obj_coordinate["xmax"]*Width/detect_size)
+    ymax = int(obj_coordinate["ymax"]*Height/detect_size)
+
+    cv2.rectangle(img, (xmin, ymin), (xmax, ymax), (255, 0, 0))
+
 # left lines, right lines
 def divide_left_right(lines):
     global Width
-    low_slope_threshold = 0
+    low_slope_threshold = 0.3
     high_slope_threshold = 10
 
     slopes = []
@@ -128,25 +156,34 @@ def divide_left_right(lines):
     left_x_sum = 0
     right_x_sum = 0
 
+    # for j in range(len(slopes)):
+    #     Line = new_lines[j]
+    #     slope = slopes[j]
+    #     x1, y1, x2, y2 = Line
+    #     if slope < 0:
+    #         left_lines.append([Line.tolist()])
+    #         left_x_sum += (x1 + x2)/2
+    #     else:
+    #         right_lines.append([Line.tolist()])
+    #         right_x_sum += (x1 + x2)/2
+
+    # if len(left_lines) != 0 and len(right_lines) != 0:
+    #     left_x_avg = left_x_sum / len(left_lines)
+    #     right_x_avg = right_x_sum / len(right_lines)
+
+    #     if left_x_avg > right_x_avg:
+    #         left_lines = []
+    #         right_lines = []
+
     for j in range(len(slopes)):
         Line = new_lines[j]
         slope = slopes[j]
         x1, y1, x2, y2 = Line
-        if slope < 0:
+        if slope < 0 and (x2 < Width/2 + 40):
             left_lines.append([Line.tolist()])
-            left_x_sum += (x1 + x2)/2
-        else:
+        elif slope > 0 and (x1 > Width/2 - 40):
             right_lines.append([Line.tolist()])
-            right_x_sum += (x1 + x2)/2
-
-    if len(left_lines) != 0 and len(right_lines) != 0:
-        left_x_avg = left_x_sum / len(left_lines)
-        right_x_avg = right_x_sum / len(right_lines)
-
-        if left_x_avg > right_x_avg:
-            left_lines = []
-            right_lines = []
-
+    
     return left_lines, right_lines
 
 
@@ -244,29 +281,30 @@ def process_image(frame):
     # draw rectangle
     frame = draw_rectangle(frame, lpos, rpos, offset=Offset)
 
+    # draw bbox
+    draw_bbox(frame)
     # show image
-    #cv2.imshow('calibration', frame)
+    # cv2.imshow('calibration', frame)
 
     return lpos, rpos, l_slope, r_slope
 
 def get_stop_line(frame):
-    global Width
-    global Offset, Gap
-    # gray
+    
+    offset1 = 380
+    gap = 20
     gray = cv2.cvtColor(frame,cv2.COLOR_BGR2GRAY)
-    roi = gray[Offset : Offset+Gap, 0 : Width]
-    # blur
-    kernel_size = 5
-    blur_roi = cv2.GaussianBlur(roi,(kernel_size, kernel_size), 0)
+    roi = gray[offset1:offset1+gap+1, :]
 
-    if cv2.mean(blur_roi) < 50:# TODO: varify this
+    num = (roi<100)
+    if num.sum() > 2600:
         return True
-
     return False
 
 def get_stop_sign(frame):
     global Width, Height
     global obj_coordinate, detect_size
+    if obj_coordinate is None:
+        return False
     x_ratio = float(Width)/float(detect_size)
     y_ratio = float(Height)/float(detect_size)
     xmin = obj_coordinate['xmin']
@@ -278,13 +316,21 @@ def get_stop_sign(frame):
     w = int((xmax-xmin)*x_ratio)
     h = int((ymax-ymin)*y_ratio)
     traffic_light_roi = frame[y:y+h, x:x+w]
-    # TODO: get red sign in traffic sign
+    
+    offset = traffic_light_roi.shape[0]/3
+    green_box = traffic_light_roi[:offset, ...]
+    yellow_box = traffic_light_roi[offset:offset*2, ...]
+    red_box = traffic_light_roi[offset*2:, ...]
+    
+    mean = np.array([np.mean(green_box), np.mean(yellow_box), np.mean(red_box)])
+    if mean.argmax() == 2:
+        return True
+    else:
+        return False 
 
-    return False
 
 
-
-def get_steer_angle(curr_position):
+def get_steer_angle(curr_position, lslope, rslope):
     # Lane tracking algorithm here
 
     k = 1.0
@@ -314,18 +360,21 @@ def velocity_control(l_slope, r_slope):
     return speed
 
 
-def drive_normal(direction=None, time=1.0):
-    print("drive_normal")
+def drive_normal(direction=None, time=0.0):
     global pub
     global image
     global sleep_rate
     global Width, Height
-    global xycar_motor_msg, steer_mm
-    global prev_angle, steering_gain
-
+    global xycar_motor_msg
+    global steer_mm, steering_gain
+    global prev_angle
+    
+    
+    steering_gain = 0.8
+    steer_mm = MovingAverage(10)
     rate = rospy.Rate(sleep_rate)
     cnt = 0
-    if time == 1.0:
+    if time == 0.0:
         max_cnt = 1# drive only one loop
     else:
         max_cnt = sleep_rate * time# drive 'time' seconds
@@ -333,25 +382,32 @@ def drive_normal(direction=None, time=1.0):
     while cnt < max_cnt:
         while not image.size == (640*480*3):
             continue
+        print("drive_normal")
 
         lpos, rpos, lslope, rslope = process_image(image)
+        
         if direction is not None and direction is "left":
-            rpos = lpos + 440
+            rpos = lpos + 470
         elif direction is not None and direction is "right":
-            lpos = rpos - 440
+            lpos = rpos - 470
+
         position = (float(lpos + rpos - Width)) / Width
 
-        steer_angle = steering_gain * get_steer_angle(position)
+        steer_angle = steering_gain * get_steer_angle(position, lslope, rslope)
+        if steer_angle < 0 and rpos == 640:
+            steer_angle = -10
+        elif steer_angle > 0 and lpos == 0:
+            steer_angle = 10
 
         if lpos == 0 and rpos == 640:
             steer_angle = prev_angle
 
         steer_mm.add_sample(steer_angle)
         wmm_angle = steer_mm.get_wmm()
-        speed = 15
+        speed = 5
         xycar_motor_msg.angle = wmm_angle
         xycar_motor_msg.speed = speed
-
+        # print("lpos = ", lpos, "rpos = ", rpos, "position = ", position, "angle = ", steer_angle, "wmm_angle = ", wmm_angle)
         pub.publish(xycar_motor_msg)
 
         prev_angle = steer_angle
@@ -362,85 +418,66 @@ def drive_normal(direction=None, time=1.0):
 
 def drive_left():
     print("drive_left")
-    drive_normal(direction="left", time=3.0)
+    drive_normal(direction="left", time=2.5)
 
 
 def drive_right():
     print("drive_right")
-    drive_normal(direction="right", time=3.0)
+    drive_normal(direction="right", time=2.5)
 
 
-def drive_stop(time=5.0):
+def drive_stop():
     print("drive_stop")
+    stop_proc()
+
+def stop_proc(time=5.0):
+    # time is max trial time(seconds) to stop
+    print("stop_process")
     global pub
     global image
     global sleep_rate
     global xycar_motor_msg
     global prev_angle
+    global obj_id
 
-    rate = rospy.Rate(sleep_rate)
-    cnt = 0
-    if time == 1.0:
-        max_cnt = 1# only one loop, it will 1/sleep_rate seconds
-    else:
-        max_cnt = sleep_rate * time# stop 'time' seconds
-
-    while cnt < max_cnt:
+    find_stop_line = False
+    proc_count = 0
+    max_count = time * sleep_rate
+    
+    while proc_count < max_count:
         while not image.size == (640*480*3):
             continue
-
-        if get_stop_line(image):
+        print("process count", proc_count)
+        if find_stop_line and get_stop_line(image):
+            print("stop!!!")
             xycar_motor_msg.angle = prev_angle
             xycar_motor_msg.speed = 0
             pub.publish(xycar_motor_msg)
+            time.sleep(5)
+            break# stop process will be end after sleep 5 seconds
+
+        if (obj_id == 2 or obj_id == 3):
+            find_stop_line = True
+        elif obj_id == 5 and get_stop_sign(image):
+            find_stop_line = True
         else:
-            drive_normal()
-            cnt -= 1
-        cnt += 1
-        rate.sleep()
+            drive_normal()# it takes 1/sleep_rate seconds
+        
+        proc_count += 1
     
     drive_normal(time=2.0)
 
-
 def find_cross_walk():
     print("find_cross_walk")
-    drive_stop()
+    stop_proc()
     
 def find_u_turn():
     print("find_u_turn")
     drive_normal()
 
-def find_traffic_light(time=5.0):
+def find_traffic_light():
     print("find_traffic_light")
-    global pub
-    global image
-    global sleep_rate
-    global xycar_motor_msg
-    global prev_angle
-
-    rate = rospy.Rate(sleep_rate)
-    cnt = 0
-    if time == 1.0:
-        max_cnt = 1# only one loop, it will 1/sleep_rate seconds
-    else:
-        max_cnt = sleep_rate * time# stop 'time' seconds
-
-    while cnt < max_cnt:
-        while not image.size == (640*480*3):
-            continue
-
-        if get_stop_sign(image):
-            xycar_motor_msg.angle = prev_angle
-            xycar_motor_msg.speed = 0
-            pub.publish(xycar_motor_msg)
-            # drive_stop()
-        else:
-            drive_normal()
-            cnt -= 1
-        cnt += 1
-        rate.sleep()
-
-    drive_normal(time=2.0)
+    stop_proc()
 
 
 def start():
@@ -450,25 +487,26 @@ def start():
     global prev_angle
     global steer_mm, steering_gain
     global sleep_rate
+    global obj_id
 
-    steer_mm = MovingAverage(15)
+    time.sleep(9)
 
     prev_angle = 0
-    steering_gain = 0.8
-    sleep_rate = 10
+    sleep_rate = 12# while loop will iterate "sleep_rate" times in a second
 
     rospy.init_node('auto_drive')
     pub = rospy.Publisher('xycar_motor', xycar_motor, queue_size=1)
 
     image_sub = rospy.Subscriber("/usb_cam/image_raw", Image, img_callback)
-    rospy.Subscriber('/yolov3_trt_ros/detections', BoundingBoxes, yolo_trt_callback, queue_size=1)
+    rospy.Subscriber('/yolov3/detections', BoundingBoxes, yolo_trt_callback, queue_size=1)
     print "---------- Xycar A2 v1.0 ----------"
 
     while True:
         while not image.size == (640*480*3):
             continue
-
-        global obj_id
+        
+        # print("x1: {}, y1: {}, x2: {}, y2: {}".format(round(obj)))
+        print("start loop obj id",obj_id)
         if obj_id != -1:
             if obj_id == 0:
                 drive_left()
